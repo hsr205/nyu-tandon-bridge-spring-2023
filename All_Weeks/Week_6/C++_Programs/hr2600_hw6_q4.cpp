5483a91de7d46b79980b5daec8924a4e7df5ee36
Henry Rothenberg <hsr205@aol.com>
2023-08-15 16:50:40 -0400

    Complete hw6_q4

--- All_Weeks/Week_6/C++_Programs/hr2600_hw6_q4.cpp
+++ All_Weeks/Week_6/C++_Programs/hr2600_hw6_q4.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+void printDivisors(int num)
+{
+
+    int secondHalfValues = 1;
+
+    for (int i = 1; i < sqrt(num); i++)
+    {
+
+        if (num % i == 0)
+        {
+            cout << i << " ";
+        }
+    }
+
+    for (int i = sqrt(num); i >= 1; i--)
+    {
+
+        if (num % i == 0)
+        {
+            secondHalfValues = num / i;
+            cout << secondHalfValues << " ";
+        }
+    }
+    cout << endl;
+}
 
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    int num;
+    cout << "Please enter a positive integer >= 2: ";
+    cin >> num;
+
+    printDivisors(num);
+
     return 0;
 }
