e0614b68baa8070ffac30b78d50e22424afade8d
Henry Rothenberg <hsr205@aol.com>
2023-08-15 16:16:11 -0400

    Complete hw6_q3

--- All_Weeks/Week_6/C++_Programs/hr2600_hw6_q3.cpp
+++ All_Weeks/Week_6/C++_Programs/hr2600_hw6_q3.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
-int main()
+double eApprox(int num)
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    double result = 1;
+    double eApproximation = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+        eApproximation += 1 / result;
+    }
+
+    return eApproximation;
+}
+
+int main()
+{
+    int n = 15;
+
+    cout.precision(30);
+
+    for (int n = 1; n <= 15; n++)
+    {
+        cout << "n = " << n << '\t' << eApprox(n) << endl;
+    }
+
     return 0;
 }
