938698645d0380ee775d0fe72802f884f9d2317e
Henry Rothenberg <hsr205@aol.com>
2023-08-15 15:07:59 -0400

    Complete hw6_q1

--- All_Weeks/Week_6/C++_Programs/hr2600_hw6_q1.cpp
+++ All_Weeks/Week_6/C++_Programs/hr2600_hw6_q1.cpp
@@ -2,9 +2,41 @@
 
 using namespace std;
 
+int fib(int num)
+{
+
+    int currentValue = 1;
+    int previousValue = 1;
+    int total = 0;
+
+    if (num == 1 || num == 2)
+    {
+        return 1;
+    }
+
+    for (int i = 1; i < (num - 1); i++)
+    {
+
+        total = currentValue + previousValue;
+        previousValue = currentValue;
+        currentValue = total;
+
+        if (i == (num - 2))
+        {
+            return total;
+        }
+    }
+
+    return 0;
+}
+
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    int inputInt;
+    cout << "Please enter a positive integer: ";
+    cin >> inputInt;
+    cout << fib(inputInt) << endl;
+
     return 0;
 }
