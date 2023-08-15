283c29b631f58f1eb089b148a719536a2bc5e0d2
Henry Rothenberg <hsr205@aol.com>
2023-08-15 15:52:15 -0400

    WIP hw6_q2

--- All_Weeks/Week_6/C++_Programs/hr2600_hw6_q2.cpp
+++ All_Weeks/Week_6/C++_Programs/hr2600_hw6_q2.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
+void printShiftedTriangle(int n, int m, char symbol)
+{
+
+    for (int i = 1; i < (n * 2); i++)
+    {
+        // TODO: Find way to center the symbols of the triangle
+
+        if (i % 2 != 0)
+        {
+            cout << string(m, ' ') << string(i, symbol) << endl;
+        }
+    }
+}
+
+void printPineTree(int n, char symbol)
+{
+
+    for (int i = 1; i < (n + 1); i++)
+    {
+        printShiftedTriangle(n+i, 4 - i, symbol);
+    }
+
+}
+
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    printShiftedTriangle(3, 4, '+');
+    // printPineTree(3, '#');
     return 0;
 }
