15cd4dbe91b3757eeedde8ade36ccd3fa751710f
Henry Rothenberg <hsr205@aol.com>
2023-08-21 09:49:38 -0400

    WIP hw7_q2

--- All_Weeks/Week_7/C++_Programs/hr2600_hw7_q2.cpp
+++ All_Weeks/Week_7/C++_Programs/hr2600_hw7_q2.cpp
@@ -1,10 +1,75 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main()
+bool isPerfect(int num)
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    int sum = 0;
+
+    for (int i = 1; i < num; i++)
+    {
+        // if (num % i == 0)
+        // {
+        sum += i;
+        if (sum == num)
+        {
+            return 1;
+        }
+        // }
+    }
+
+    return 0;
+}
+
+bool isAmicableNum(int originalNum, int sumOfDivisors)
+{
+
+    if (originalNum == sumOfDivisors)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+void analyzeDividors(int num, int &outCountDivs, int &outSumDivs)
+{
+
+    for (int i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            if (isPerfect(num))
+            {
+                // cout << num << endl;
+                cout << "The integer " << num << " is a perfect number" << endl;
+            }
+            // cout << "i: " << i << endl;
+            outCountDivs++;
+            outSumDivs += i;
+        }
+    }
+
+    if (isAmicableNum(num, outSumDivs))
+    {
+        cout << "The two integers: " << num << " and " << outSumDivs << " are amicable numbers." << endl;
+    }
+}
+
+int main()
+{
+    int inputValue = 6;
+    int outCountDivs = 0;
+    int outSumDivs = 0;
+
+    // bool isResult = isPerfect(inputValue);
+
+    analyzeDividors(inputValue, outCountDivs, outSumDivs);
+
+    // cout << "outCountDivs: " << outCountDivs << endl;
+    // cout << "outSumDivs: " << outSumDivs << endl;
+
     return 0;
 }
