0f50169075410ad45d2af53ad895472eb8350e2a
Henry Rothenberg <hsr205@aol.com>
2023-08-22 17:11:18 -0400

    WIP hw8_q6

--- All_Weeks/Week_8/C++_Programs/hr2600_hw8_q6.cpp
+++ All_Weeks/Week_8/C++_Programs/hr2600_hw8_q6.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+void getUserInput()
+{
+    string inputStr = "My userid is john17 and my 4 digit pin is 1234 which is secret";
+    cout << inputStr << endl;
+
+    int indexStart = 0;
+    int indexPosition = 0;
+    string currentWord;
+    string lastWord;
+    string newSentence;
+
+    for (int i = 0; i < inputStr.length(); i++)
+    {
+        if (inputStr[i] == ' ')
+        {
+            currentWord = inputStr.substr(indexStart, i - indexStart);
+
+            indexPosition = currentWord.find_first_of("0123456789");
+            if (indexPosition == 0)
+            {
+                currentWord = string(currentWord.length(), 'x');
+            }
+            newSentence += currentWord + " ";
+            indexStart = i + 1;
+        }
+    }
+
+    indexPosition = 0;
+
+    lastWord = inputStr.substr(indexStart);
+
+    indexPosition = lastWord.find_first_of("0123456789");
+
+    if (indexPosition == 0)
+    {
+        lastWord = string(lastWord.length(), 'x');
+    }
+
+    cout << newSentence + lastWord << endl;
+}
 
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    getUserInput();
     return 0;
 }
