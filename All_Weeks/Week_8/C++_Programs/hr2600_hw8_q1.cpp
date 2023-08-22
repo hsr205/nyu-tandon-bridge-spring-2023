40bb6c28002f20fba5f982cfaa25b4b95cf87eab
Henry Rothenberg <hsr205@aol.com>
2023-08-22 10:41:27 -0400

    Complete hw8_q1

--- All_Weeks/Week_8/C++_Programs/hr2600_hw8_q1.cpp
+++ All_Weeks/Week_8/C++_Programs/hr2600_hw8_q1.cpp
@@ -2,9 +2,59 @@
 
 using namespace std;
 
+const int ARRAY_SIZE = 20;
+
+int minInArray(int arr[], int arrSize)
+{
+    int smallestValue = arr[0];
+
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (arr[i] < smallestValue)
+        {
+            smallestValue = arr[i];
+        }
+    }
+
+    return smallestValue;
+}
+
+void findIndicesOfMinArrayValue(int arr[], int minArrayValue)
+{
+
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        if (minArrayValue == arr[i])
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+void getResult()
+{
+
+    int inputValue;
+    int arr[ARRAY_SIZE];
+    cout << "Please enter " << ARRAY_SIZE << " integers separated by a space: " << endl;
+
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        cin >> inputValue;
+        arr[i] = inputValue;
+    }
+
+    int smallestValue = minInArray(arr, ARRAY_SIZE);
+
+    cout << "The minimum value is " << smallestValue << ", and it is located in the following indices: ";
+    findIndicesOfMinArrayValue(arr, smallestValue);
+}
+
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    getResult();
+
     return 0;
 }
