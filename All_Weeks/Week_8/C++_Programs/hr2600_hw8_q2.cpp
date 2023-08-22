e41ba432086e7335919d69820fd9c7db978ebfd0
Henry Rothenberg <hsr205@aol.com>
2023-08-22 10:57:30 -0400

    Complete hw8_q2

--- All_Weeks/Week_8/C++_Programs/hr2600_hw8_q2.cpp
+++ All_Weeks/Week_8/C++_Programs/hr2600_hw8_q2.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+bool isPalindrome(string str)
+{
+    string reversedString;
+
+    for (int i = (str.length() - 1); i >= 0; i--)
+    {
+        reversedString += str[i];
+    }
+
+    if (str == reversedString)
+    {
+        cout << str << " is a palidrome" << endl;
+        return 1;
+    }
+
+    cout << str << " is NOT a palidrome" << endl;
+    return 0;
+}
+
+void getResult()
+{
+    string userInputStr;
+
+    cout << "Please enter a word: ";
+    cin >> userInputStr;
+
+    isPalindrome(userInputStr);
+}
+
 int main()
 {
-
-    cout << "Hello from: " << __FILE__ << endl;
+    getResult();
     return 0;
 }
