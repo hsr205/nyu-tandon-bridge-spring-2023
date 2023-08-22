e1ec67698e6e11c1bf4c5ae79a5b5a1f9cf26b82
Henry Rothenberg <hsr205@aol.com>
2023-08-22 16:26:13 -0400

    Complete hw8_q5

--- All_Weeks/Week_8/C++_Programs/hr2600_hw8_q5.cpp
+++ All_Weeks/Week_8/C++_Programs/hr2600_hw8_q5.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
+void getResult()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    string firstName;
+    string middleName;
+    string lastName;
+
+    cout << "Please enter your first, middle and last names in that order: " << endl;
+
+    cin >> firstName;
+    cin >> middleName;
+    cin >> lastName;
+
+    cout << lastName << ", " << firstName << " " << middleName.substr(0, 1) + "." << endl;
+}
+
+int main()
+{
+    getResult();
     return 0;
 }
