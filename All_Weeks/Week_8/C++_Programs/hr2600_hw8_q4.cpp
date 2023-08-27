b53dc7de482b38afa77cef4b0242cc9a16016dce
Henry Rothenberg <hsr205@aol.com>
2023-08-27 17:18:12 -0400

    Add Syllabus

--- All_Weeks/Week_8/C++_Programs/hr2600_hw8_q4.cpp
+++ All_Weeks/Week_8/C++_Programs/hr2600_hw8_q4.cpp
@@ -1,66 +1,138 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <map>
 
 using namespace std;
 
-const int ARRAY_SIZE = 10;
 int pinNum = 12345;
 
-void assignRandomValues()
+int *intToArray(int inputValue)
 {
 
-    srand(static_cast<int>(time(nullptr)));
-
     int cnt = 0;
-    int pinArray[ARRAY_SIZE];
-    int randomArray[ARRAY_SIZE];
-    int sequencialArray[ARRAY_SIZE];
 
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    vector<int> collection;
+    while (inputValue > 0)
     {
-        randomArray[i] = rand() % 3 + 1;
+        int digit = inputValue % 10;
+
+        inputValue /= 10;
+        collection.push_back(digit);
     }
 
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        sequencialArray[i] = i;
-    }
+    int *arr = new int[collection.size()];
 
-    while (pinNum > 0)
+    for (int i = (collection.size() - 1); i >= 0; i--)
     {
-
-        int digit = pinNum % 10;
-        pinArray[cnt] = digit;
-        pinNum /= 10;
+        arr[cnt] = collection[i];
         cnt++;
     }
 
-//     for (int i = 0; i < ARRAY_SIZE; i++)
-//     {
-//         // cout << "randomArray[i]: " << randomArray[i] << endl;
-//         // cout << "sequencialArray[i]: " << sequencialArray[i] << endl;
+    return arr;
+}
+map<int, int> getMap()
+{
+    srand(static_cast<int>(time(nullptr)));
 
-//     }
-// }
+    map<int, int> map;
+
+    for (int i = 0; i < 10; i++)
+    {
+        map[i] = rand() % 3 + 1;
+    }
+
+    return map;
+}
+
+void getResultVector(int digit, vector<int> &collection)
+{
+    map<int, int> map = getMap();
+
+    for (const auto &keyValue : map)
+    {
+        if (digit == keyValue.first)
+        {
+            collection.push_back(keyValue.second);
+            cout << digit << " -> " << keyValue.second << endl;
+        }
+    }
+}
+
+vector<int> getCodedPin(int inputValue)
+{
+
+    vector<int> collection;
+
+    int *codedPin = intToArray(inputValue);
+
+    for (int i = 0; i < 5; i++)
+    {
+        int digit = codedPin[i];
+        getResultVector(digit, collection);
+    }
+
+    return collection;
+}
+
+void printInterfaceValues()
+{
+    map<int, int> map = getMap();
+
+    cout << "PIN: ";
+    for (const auto &keyValue : map)
+    {
+        cout << keyValue.first << string(2, ' ');
+    }
+    cout << endl;
+
+    cout << "NUM: ";
+    for (const auto &keyValue : map)
+    {
+        cout << keyValue.second << string(2, ' ');
+    }
+    cout << endl;
+}
 
 void getUserInput()
 {
 
-    // int inputValue;
+    int inputValue;
 
-    // cout << "Please enter your PIN according to the following mapping: " << endl;
+    cout << "Please enter your PIN according to the following mapping: " << endl;
+    printInterfaceValues();
 
-    // assignRandomValues();
+    cin >> inputValue;
 
-    // cin >> inputValue;
+    vector<int> codedPinVector = getCodedPin(pinNum);
+    int *inputValueArray = intToArray(inputValue);
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (codedPinVector[i] != inputValueArray[i])
+        {
+            cout << "Your PIN is incorrect" << endl;
+        }
+        else
+        {
+            cout << "Your PIN is correct" << endl;
+        }
+    }
 }
 
 int main()
 {
+    // vector<int> codedPin = getCodedPin(pinNum);
 
-    // getUserInput();
-    assignRandomValues();
+    // cout << "Coded Pin: ";
+    // for (int element : codedPin)
+    // {
+    //     cout << element;
+    // }
+    // cout << endl;
+    getUserInput();
+
     return 0;
 }
