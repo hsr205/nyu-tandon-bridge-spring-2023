dc0bcea4cad284ca29ef5b426f9cf4e50f6817c7
Henry Rothenberg <hsr205@aol.com>
2023-08-29 10:49:54 -0400

    WIP hw9_q3

--- All_Weeks/Week_9/C++_Programs/hr2600_hw9_q3.cpp
+++ All_Weeks/Week_9/C++_Programs/hr2600_hw9_q3.cpp
@@ -2,9 +2,44 @@
 
 using namespace std;
 
+const int ARRAY_SIZE = 6;
+
+int *getPosNums1(int *arr, int arrSize, int &outPosArrSize)
+{
+
+    int resultArray[ARRAY_SIZE];
+
+    for (int i = 0; i < arrSize; i++)
+    {
+        // cout << "arr[i]: " << arr[i] << endl;
+        if (arr[i] > 0)
+        {
+            cout << "arr[i]: " << arr[i] << endl;
+            resultArray[outPosArrSize] = arr[i];
+            outPosArrSize++;
+        }
+    }
+
+    return resultArray;
+}
+
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    int outPosArrSize = 0;
+    // int arrSize = ARRAY_SIZE;
+    int arr[ARRAY_SIZE] = {3, -1, -3, 0, 6, 4};
+
+    cout << "outPosArrSize: " << outPosArrSize << endl;
+
+    int *resultArray = getPosNums1(arr, ARRAY_SIZE, outPosArrSize);
+
+    cout << "outPosArrSize: " << outPosArrSize << endl;
+
+    // for (int i = 0; i < outPosArrSize; i++)
+    // {
+    //     cout << "resultArray[i]: " << resultArray[i] << endl;
+    // }
+
     return 0;
 }
