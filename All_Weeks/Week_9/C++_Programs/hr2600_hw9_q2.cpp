db9d7aacb2372ba9c31ceba8b35d67574726c43f
Henry Rothenberg <hsr205@aol.com>
2023-08-29 10:19:32 -0400

    Complete hw9_q2

--- All_Weeks/Week_9/C++_Programs/hr2600_hw9_q2.cpp
+++ All_Weeks/Week_9/C++_Programs/hr2600_hw9_q2.cpp
@@ -1,10 +1,88 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
-int main()
+map<char, int> getMapFromString(string inputString)
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    map<char, int> resultMap;
+
+    for (char character : inputString)
+    {
+        bool isNotSpace = !isspace(character);
+        bool isAlpha = isalpha(character);
+
+        if (isNotSpace && isAlpha)
+        {
+            character = tolower(character);
+            resultMap[character]++;
+        }
+    }
+
+    return resultMap;
+}
+
+bool isAnagram(map<char, int> map1, map<char, int> map2)
+{
+
+    int cnt = 0;
+    auto iterator1 = map1.begin();
+    auto iterator2 = map2.begin();
+    bool isSameSize = map1.size() == map2.size();
+
+    if (isSameSize)
+    {
+        while (iterator1 != map1.end() && iterator2 != map2.end())
+        {
+            bool isKeysEqual = iterator1->first == iterator2->first;
+            bool isValuesEqual = iterator1->second == iterator2->second;
+
+            if (isKeysEqual && isValuesEqual)
+            {
+                cnt++;
+                if (cnt == map1.size() && cnt == map2.size())
+                {
+                    return 1;
+                }
+            }
+
+            ++iterator1;
+            ++iterator2;
+        }
+    }
+
+    return 0;
+}
+
+void getUserInput()
+{
+
+    string stringInput1;
+    string stringInput2;
+
+    cout << "Please enter two sentences: " << endl;
+    getline(cin, stringInput1);
+    getline(cin, stringInput2);
+
+    map<char, int> map1 = getMapFromString(stringInput1);
+    map<char, int> map2 = getMapFromString(stringInput2);
+
+    if (isAnagram(map1, map2))
+    {
+
+        cout << "The two sentences are anagrams" << endl;
+    }
+    else
+    {
+        cout << "The two sentences are not anagrams" << endl;
+    }
+}
+
+int main()
+{
+    getUserInput();
+
     return 0;
 }
