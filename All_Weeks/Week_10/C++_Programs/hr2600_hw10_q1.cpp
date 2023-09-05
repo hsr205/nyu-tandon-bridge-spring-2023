09aa7e52e59c1482c5dd3e9181854fd536e0bb35
Henry Rothenberg <hsr205@aol.com>
2023-09-05 10:21:06 -0400

    Complete hw10_q1

--- All_Weeks/Week_10/C++_Programs/hr2600_hw10_q1.cpp
+++ All_Weeks/Week_10/C++_Programs/hr2600_hw10_q1.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+string *createWordsArray(string sentence, int &outWordsArrSize)
+{
+    int indexStart = 0;
+    string lastWord;
+    string currentWord;
+    string *stringArr = new string[outWordsArrSize];
+
+    for (int i = 0; i < sentence.length(); i++)
+    {
+
+        if (sentence[i] == ' ')
+        {
+
+            currentWord = sentence.substr(indexStart, i - indexStart);
+            stringArr[outWordsArrSize] = currentWord;
+            indexStart = i + 1;
+            outWordsArrSize++;
+        }
+    }
+
+    lastWord = sentence.substr(indexStart);
+
+    stringArr[outWordsArrSize] = lastWord;
+
+    outWordsArrSize++;
+
+    return stringArr;
+}
 
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    int outWordsArrSize = 0;
+    string sentence = "You can do it";
+
+    cout << "outWordsArrSize: " << outWordsArrSize << endl;
+
+    string *result = createWordsArray(sentence, outWordsArrSize);
+
+    cout << "outWordsArrSize: " << outWordsArrSize << endl;
+
+    for (int i = 0; i < outWordsArrSize; i++)
+    {
+        cout << "result[i]: " << result[i] << endl;
+    }
+
     return 0;
 }
