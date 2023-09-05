36bbe000de7b7447730e33a41bdd96dd93c86fae
Henry Rothenberg <hsr205@aol.com>
2023-09-05 10:48:50 -0400

    Complete hw10_q2

--- All_Weeks/Week_10/C++_Programs/hr2600_hw10_q2.cpp
+++ All_Weeks/Week_10/C++_Programs/hr2600_hw10_q2.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
-int main()
+int *findMissing(int arr[], int n, int &resArrSize)
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    int *result = new int[resArrSize];
+
+    set<int> collectionSet(arr, arr + n);
+
+    for (int i = 0; i <= collectionSet.size(); i++)
+    {
+        if (collectionSet.find(i) == collectionSet.end())
+        {
+            result[resArrSize] = i;
+            resArrSize++;
+        }
+    }
+
+    return result;
+}
+
+int main()
+{
+    int resArrSize = 0;
+    int arr[] = {3, 1, 3, 0, 6, 4};
+
+    cout << "resArrSize: " << resArrSize << endl;
+
+    int *resultArray = findMissing(arr, 6, resArrSize);
+
+    cout << "resArrSize: " << resArrSize << endl;
+
+    for (int i = 0; i < resArrSize; i++)
+    {
+        cout << "resultArray[i]: " << resultArray[i] << endl;
+    }
+
+    delete[] resultArray;
+
     return 0;
 }
