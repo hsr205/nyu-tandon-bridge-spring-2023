18b6f814179167b401e85a57ad8b9569599e9acf
Henry Rothenberg <hsr205@aol.com>
2023-09-06 15:45:39 -0400

    Complete part 1 hw_10_q3

--- All_Weeks/Week_10/C++_Programs/hr2600_hw10_q3.cpp
+++ All_Weeks/Week_10/C++_Programs/hr2600_hw10_q3.cpp
@@ -1,10 +1,86 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+vector<int> insertInputValuesToVector()
+{
+
+    int inputValue;
+    vector<int> resultVector;
+    cout << "Please enter a sequence of positive integers, each in a separte line." << endl;
+    cout << "End your input by typing -1." << endl;
+
+    while (true)
+    {
+
+        cin >> inputValue;
+
+        if (inputValue == -1)
+        {
+            break;
+        }
+
+        else
+        {
+            resultVector.push_back(inputValue);
+        }
+    }
+
+    return resultVector;
+}
+
+void printResult(int searchNum, vector<int> searchNumLineAppearences)
+{
+
+    if (searchNumLineAppearences.size() > 0)
+    {
+        cout << searchNum << " shows in lines ";
+        for (int i = 0; i < searchNumLineAppearences.size(); i++)
+        {
+            if (i == searchNumLineAppearences.size() - 1)
+            {
+                cout << searchNumLineAppearences[i] << ".";
+            }
+            else
+            {
+                cout << searchNumLineAppearences[i] << ", ";
+            }
+        }
+        cout << endl;
+    }
+
+    else
+    {
+        cout << searchNum << " was not found." << endl;
+    }
+}
+
+void findNumLocation1()
+{
+    int cnt = 0;
+    int searchNum;
+    vector<int> searchNumLineAppearences;
+    vector<int> inputValuesVector = insertInputValuesToVector();
+
+    cout << "Please enter you want to search." << endl;
+    cin >> searchNum;
+
+    for (int i = 0; i < inputValuesVector.size(); i++)
+    {
+        cnt++;
+        if (searchNum == inputValuesVector[i])
+        {
+            searchNumLineAppearences.push_back(cnt);
+        }
+    }
+
+    printResult(searchNum, searchNumLineAppearences);
+}
 
 int main()
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    findNumLocation1();
     return 0;
 }
