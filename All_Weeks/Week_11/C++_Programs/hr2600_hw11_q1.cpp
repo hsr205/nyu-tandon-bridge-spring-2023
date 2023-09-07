40adffb6874f1d64e6659f1f4d8d844bae5a9d15
Henry Rothenberg <hsr205@aol.com>
2023-09-07 16:20:38 -0400

    WIP hw11_q1

--- All_Weeks/Week_11/C++_Programs/hr2600_hw11_q1.cpp
+++ All_Weeks/Week_11/C++_Programs/hr2600_hw11_q1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -40,7 +42,7 @@ void printOppositeTriangles(int n)
     printTriangle(n);
 }
 
-int main()
+void getTriangleResult()
 {
     int n;
     cout << "Please enter an integer greater than 0:" << endl;
@@ -53,11 +55,41 @@ int main()
     printTriangle(n);
     cout << "----------------------------------------" << endl;
     cout << endl;
-    
+
     cout << "Testing printOppositeTriangles() method: " << endl;
     cout << "----------------------------------------" << endl;
     printOppositeTriangles(n);
     cout << "----------------------------------------" << endl;
+}
 
+void printRuler(int n)
+{
+    /*
+
+        What the question is asking for is the following:
+
+        (1) If half of "n^2" then print out "n" number of "-"
+        (2) If "n^2" / "n" can half a simplified fraction then print out either n/2 or n-1 number of "-"
+        (3) Else print out a single "-"
+    
+    */
+    if (n != 0)
+    {
+        int powerValue = pow(n, 2);
+        // cout << "powerValue: " << powerValue << endl;
+        // cout << "n: " << n << endl;
+        if (powerValue / n == n)
+        {
+            cout << string(n,'-');
+        }
+        // cout << endl;
+        // cout << "pow(n,2) - 1: " << pow(n,2) - 1 << endl;
+        printRuler(n - 1);
+    }
+}
+
+int main()
+{
+    printRuler(2);
     return 0;
 }
