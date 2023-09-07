a63456d60deade3927353191de9893f7b1c0dbc2
Henry Rothenberg <hsr205@aol.com>
2023-09-07 15:47:01 -0400

    WIP hw11_q1

--- All_Weeks/Week_11/C++_Programs/hr2600_hw11_q1.cpp
+++ All_Weeks/Week_11/C++_Programs/hr2600_hw11_q1.cpp
@@ -2,9 +2,62 @@
 
 using namespace std;
 
+int printTriangle(int n)
+{
+    int recursiveValue = 0;
+    if (n == 0)
+    {
+        return n;
+    }
+    else
+    {
+        recursiveValue = printTriangle(n - 1) + 1;
+        for (int i = 0; i < recursiveValue; i++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        return recursiveValue;
+    }
+}
+
+void printFlippedTriangle(int n)
+{
+    if (n != 0)
+    {
+        for (int i = n; i > 0; i--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        printFlippedTriangle(n - 1);
+    }
+}
+
+void printOppositeTriangles(int n)
+{
+    printFlippedTriangle(n);
+    printTriangle(n);
+}
+
 int main()
 {
+    int n;
+    cout << "Please enter an integer greater than 0:" << endl;
+    cin >> n;
 
-    cout << "Hello from: " << __FILE__ << endl;
+    cout << endl;
+
+    cout << "Testing printTriangle() method: " << endl;
+    cout << "----------------------------------------" << endl;
+    printTriangle(n);
+    cout << "----------------------------------------" << endl;
+    cout << endl;
+    
+    cout << "Testing printOppositeTriangles() method: " << endl;
+    cout << "----------------------------------------" << endl;
+    printOppositeTriangles(n);
+    cout << "----------------------------------------" << endl;
+
     return 0;
 }
