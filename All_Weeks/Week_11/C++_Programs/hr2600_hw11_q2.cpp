a4cad6e36c92c3bcd36fd66911216b4aeca2516e
Henry Rothenberg <hsr205@aol.com>
2023-09-15 11:22:12 -0400

    WIP hw_11_q2

--- All_Weeks/Week_11/C++_Programs/hr2600_hw11_q2.cpp
+++ All_Weeks/Week_11/C++_Programs/hr2600_hw11_q2.cpp
@@ -13,20 +13,7 @@ void printArray(int arr[], int arrSize)
     }
 }
 
-// int sumOfSquares(int arr[], int arrSize)
-// {
-
-//     for (int i = 0; i < arrSize; i++)
-//     {
-//         arr[i] = pow(arr[i], 2);
-//     }
-
-//     printArray(arr, arrSize);
-
-//     return 0;
-// }
-
-// TODO: Redo this
+// TODO: Redo this to fiully understand
 int sumOfSquares(int arr[], int arrSize)
 {
     if (arrSize == 0)
@@ -37,9 +24,90 @@ int sumOfSquares(int arr[], int arrSize)
     {
 
         int currentElement = arr[arrSize - 1];
-        int square = currentElement * currentElement;
+        // cout << "currentElement: " << currentElement << endl;
+        // sumOfSquares(arr, arrSize - 1);
+        int square = pow(currentElement, 2);
+        // cout << "square: " << square << endl;
         int sum = square + sumOfSquares(arr, arrSize - 1);
+        // cout << "sum: " << sum << endl;
         return sum;
+    }
+}
+
+// int addDigits(int *arr, int arrSize)
+// {
+
+//     if (arrSize == 1)
+//     {
+//         return arr[0];
+//     }
+//     else
+//     {
+//         int temp;
+//         temp = addDigits(arr, arrSize - 1);
+//         return temp + arr[arrSize - 1];
+//     }
+// }
+
+// int getPowOf(int a, int n)
+// {
+
+//     if (n == 0)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         int temp;
+//         temp = getPowOf(a, n - 1) * a;
+//         return temp;
+//     }
+// }
+
+bool isSortedNonRecursive(int arr[], int arrSize)
+{
+    bool result = false;
+
+    for (int i = 0; i < arrSize - 1; i++)
+    {
+        if (arr[i] <= arr[i + 1])
+        {
+            result = true;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    return result;
+}
+
+bool isSorted(int arr[], int arrSize)
+{
+    bool result = false;
+
+    if (arrSize == 0)
+    {
+        // cout << "Inside Conditional: " << endl;
+        // cout << "arr[arrSize]: " << arr[arrSize-1] << endl;
+        // result = arr[arrSize - 1] >= arr[arrSize - 2];
+
+        return result;
+    }
+    else
+    {
+        // cout << "arr[arrSize]: " << arr[arrSize] << endl;
+        cout << "arr[arrSize-1]: " << arr[arrSize-1] << endl;
+        cout << "arr[arrSize-2]: " << arr[arrSize-2] << endl;
+        bool result = arr[arrSize - 1] >= arr[arrSize - 2];
+        cout << "arr[arrSize - 1] >= arr[arrSize - 2]: " << result << endl;
+        cout << "-----------------------------------------------------------" << endl;
+
+        // result = arr[arrSize - 1] >= arr[arrSize - 2];
+        // isSorted(arr, arrSize - 1);
+
+        return isSorted(arr, arrSize - 1);
     }
 }
 
@@ -47,12 +115,13 @@ int main()
 {
 
     int arr[ARRAY_SIZE] = {2, -1, 3, 10};
+    int arr2[ARRAY_SIZE] = {1, 2, 3, 4};
 
-    // sumOfSquares(arr, ARRAY_SIZE);
+    // cout << "Is array sorted: " << isSortedNonRecursive(arr2, ARRAY_SIZE) << endl;
 
-    // int result = sumOfSquares(arr, ARRAY_SIZE);
+    isSorted(arr2, ARRAY_SIZE);
 
-    cout << "result: " << sumOfSquares(arr, ARRAY_SIZE) << endl;
-    // sumOfSquares(arr, ARRAY_SIZE);
+    cout << "Is array sorted: " << isSorted(arr2, ARRAY_SIZE) << endl;
+
     return 0;
 }
