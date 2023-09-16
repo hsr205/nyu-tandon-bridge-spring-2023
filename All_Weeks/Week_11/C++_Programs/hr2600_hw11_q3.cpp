b0f9cea1aa14e65b06e95ece6b73e5a59b972bc4
Henry Rothenberg <hsr205@aol.com>
2023-09-16 13:51:04 -0400

    Complete hw11_q3 part 1

--- All_Weeks/Week_11/C++_Programs/hr2600_hw11_q3.cpp
+++ All_Weeks/Week_11/C++_Programs/hr2600_hw11_q3.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 
-int main()
+const int ARRAY_SIZE = 5;
+
+
+int minArray1(int arr[], int arrSize)
 {
 
-    cout << "Hello from: " << __FILE__ << endl;
+    if (arrSize == 0)
+    {
+
+        return arr[0];
+    }
+
+    return min(arr[0], minArray1(arr + 1, arrSize - 1));
+}
+
+int main()
+{
+    int arr[ARRAY_SIZE] = {1, -100, 3, 4, -5};
+
+    int result = minArray1(arr, ARRAY_SIZE);
+    cout << "Min Value: " << result << endl;
+
+    // minArray1(arr, ARRAY_SIZE);
+
     return 0;
 }
