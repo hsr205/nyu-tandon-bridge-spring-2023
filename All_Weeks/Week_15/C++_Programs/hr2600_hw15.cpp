6c7175fe466baf58b64298d8c7c132ab116cd79c
Henry Rothenberg <hsr205@aol.com>
2023-10-18 17:22:21 -0400

    WIP hw15

--- All_Weeks/Week_15/C++_Programs/hr2600_hw15.cpp
+++ All_Weeks/Week_15/C++_Programs/hr2600_hw15.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <list>
+#include <map>
 
 using namespace std;
 
@@ -50,11 +51,46 @@ public:
     }
 };
 
+class EmployeePayRoll
+{
+
+private:
+    int id;
+    double hoursWorked;
+
+public:
+    EmployeePayRoll(int id = 0, double hoursWorked = 0.0)
+        : id(id), hoursWorked(hoursWorked){};
+
+    ~EmployeePayRoll() = default;
+
+    int getId() const
+    {
+        return id;
+    }
+
+    double getHoursWorked() const
+    {
+        return hoursWorked;
+    }
+
+    void setId(int newId)
+    {
+        id = newId;
+    }
+
+    void setHoursWorked(double newHoursWorked)
+    {
+        hoursWorked = newHoursWorked;
+    }
+};
+
 class FileProcessing
 {
 
 private:
     Employee employee;
+    EmployeePayRoll employeePayRoll;
 
     void openInputFile(ifstream &inputFile)
     {
@@ -79,14 +115,14 @@ public:
 
     ~FileProcessing() = default;
 
-    vector<Employee> getEmployeeVector(ifstream &inputFile)
+    list<Employee> getEmployeeData(ifstream &inputFile)
     {
         openInputFile(inputFile);
 
         int id;
         double hourlyPay;
         string name;
-        vector<Employee> employeeVector;
+        list<Employee> employeeList;
 
         while (inputFile >> id >> hourlyPay)
         {
@@ -98,37 +134,112 @@ public:
             employee.setHourlyPay(hourlyPay);
             employee.setName(name);
 
-            employeeVector.push_back(employee);
+            employeeList.push_back(employee);
         }
 
         inputFile.close();
 
-        return employeeVector;
+        return employeeList;
+    }
+
+    list<EmployeePayRoll> getEmployeePayRollData(ifstream &inputFile)
+    {
+        openInputFile(inputFile);
+
+        int id;
+        int hoursWorked;
+        list<EmployeePayRoll> employeeList;
+
+        while (inputFile >> id >> hoursWorked)
+        {
+            EmployeePayRoll employeePayRoll;
+
+            employeePayRoll.setId(id);
+            employeePayRoll.setHoursWorked(hoursWorked);
+
+            employeeList.push_back(employeePayRoll);
+        }
+
+        inputFile.close();
+
+        return employeeList;
     }
 };
 
+struct AllEmployeeData
+{
+    int id;
+    bool isDuplicate;
+    double hoursWorked;
+    double hourlyPay;
+    double totalPay;
+    string name;
+};
 
-/*
-    (1) Now read in the data from the payroll.txt file
-*/
 int main()
 {
 
     Employee employee;
     ifstream inputFile;
     FileProcessing fileProcessing;
+    EmployeePayRoll employeePayRoll;
+    vector<AllEmployeeData> allEmployeeDataVector;
 
-    vector<Employee> employeeVector = fileProcessing.getEmployeeVector(inputFile);
+    list<Employee> employeeList = fileProcessing.getEmployeeData(inputFile);
+    list<EmployeePayRoll> employeePayRollList = fileProcessing.getEmployeePayRollData(inputFile);
 
-    for (const Employee &employee : employeeVector)
+    for (const Employee &employee : employeeList)
     {
-        cout << string(30, '=') << endl;
-        cout << "Id: " << employee.getId() << endl;
-        cout << "Hourly Pay: " << employee.getHourlyPay() << endl;
-        cout << "Name: " << employee.getName() << endl;
+
+        for (const EmployeePayRoll &employeePayRoll : employeePayRollList)
+        {
+
+            AllEmployeeData employeeData;
+
+            if (employee.getId() == employeePayRoll.getId())
+            {
+                // cout << "Id: " << employee.getId() << endl;
+                employeeData.id = employee.getId();
+                employeeData.name = employee.getName();
+                employeeData.hourlyPay = employee.getHourlyPay();
+                employeeData.hoursWorked = employeePayRoll.getHoursWorked();
+                employeeData.totalPay = employee.getHourlyPay() * employeePayRoll.getHoursWorked();
+            }
+
+            else
+            {
+                employeeData.name = "Duplicate";
+            }
+
+            allEmployeeDataVector.push_back(employeeData);
+        }
     }
 
-    cout << string(30, '=') << endl;
+    map<string, double> resultMap;
+
+    for (const AllEmployeeData &employee : allEmployeeDataVector)
+    {
+        if (employee.name != "Duplicate")
+        {
+            resultMap[employee.name] += employee.totalPay;
+        }
+    }
+
+    cout << endl;
+
+    cout << string(9, '*') << "Payroll Information" << string(8, '*') << endl;
+    /*
+    // TODO: 
+        (1) Find a simple way to sort the map by value in descending order
+        (2) Abstract most of the implementations in the main method into separate classes
+    */
+
+    for (const auto &keyValue : resultMap)
+    {
+        cout << keyValue.first << ", $" << keyValue.second << endl;
+    }
+
+    cout << string(9, '*') << "End Payroll" << string(14, '*') << endl;
 
     return 0;
 }
