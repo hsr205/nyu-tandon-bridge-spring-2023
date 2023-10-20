d6e6791e2c96759a999dd1b788b9458b45ba6bc7
Henry Rothenberg <hsr205@aol.com>
2023-10-20 10:21:46 -0400

    Add Payments class / remove Payments struct

--- All_Weeks/Week_15/C++_Programs/hr2600_hw15.cpp
+++ All_Weeks/Week_15/C++_Programs/hr2600_hw15.cpp
@@ -5,19 +5,80 @@
 
 using namespace std;
 
-struct Payments
+class Payments
 {
-    double paymentAmount;
+private:
     string name;
+    double paymentOwed;
+    double paymentAmount;
+
+    double getAveragePayment(list<Payments> paymentList)
+    {
+        double paymentTotal = 0.0;
+        double averagePayment = 0.0;
+
+        for (const Payments payment : paymentList)
+        {
+            paymentTotal += payment.getPaymentAmount();
+        }
+
+        averagePayment = paymentTotal / paymentList.size();
+
+        return averagePayment;
+    }
+
+public:
+    Payments(){};
+    ~Payments() = default;
+
+    string getName() const
+    {
+        return name;
+    }
+
+    double getPaymentOwed() const
+    {
+        return paymentOwed;
+    }
+
+    double getPaymentAmount() const
+    {
+        return paymentAmount;
+    }
+
+    void setName(string newName)
+    {
+        name = newName;
+    }
+
+    void setPaymentOwed(double newPaymentOwed)
+    {
+        paymentOwed = newPaymentOwed;
+    }
+
+    void setPaymentAmount(double newPaymentAmount)
+    {
+        paymentAmount = newPaymentAmount;
+    }
+
+    void populatePaymentOwedField(list<Payments> &paymentList)
+    {
+
+        double averagePayment = getAveragePayment(paymentList);
+
+        cout << "Average Payment: $" << averagePayment << endl;
+
+        for (Payments &payment : paymentList)
+        {
+            payment.paymentOwed = averagePayment - payment.paymentAmount;
+        }
+    }
 };
 
 class FileProcessing
 {
 
-public:
-    FileProcessing(){};
-    ~FileProcessing() = default;
-
+private:
     void openFile(ifstream &inputFile)
     {
 
@@ -37,6 +98,10 @@ public:
         }
     }
 
+public:
+    FileProcessing(){};
+    ~FileProcessing() = default;
+
     void getDataFile(ifstream &inputFile, list<Payments> &paymentList)
     {
         string name;
@@ -50,8 +115,9 @@ public:
 
             getline(inputFile, name);
 
-            payment.name = name;
-            payment.paymentAmount = paymentAmount;
+            payment.setName(name);
+            payment.setPaymentAmount(paymentAmount);
+            payment.setPaymentOwed(0.0);
 
             paymentList.push_back(payment);
         }
@@ -62,20 +128,39 @@ public:
 
 int main()
 {
-    double sum = 0.0;
     ifstream inputFile;
+    Payments payments;
     list<Payments> paymentList;
     FileProcessing fileProcessing;
 
     fileProcessing.getDataFile(inputFile, paymentList);
+    payments.populatePaymentOwedField(paymentList);
+
+    // getPaymentOwed(paymentList);
+
+    /*
+    TODO:
+
+    (1) Compare the payment.paymentOwed values and find a way to repay
+        those that have negative values with those that have positive values
+    */
 
     for (const Payments payment : paymentList)
     {
-        cout << payment.name << " -> " << payment.paymentAmount << endl;
-        sum += payment.paymentAmount;
-    }
 
-    cout << "Sum: " << sum << endl;
+        if (payment.getPaymentOwed() < 0)
+        {
+            cout << payment.getName() << " is owed $" << payment.getPaymentOwed() << endl;
+        }
+        else if (payment.getPaymentOwed() > 0)
+        {
+            cout << payment.getName() << " owes $" << payment.getPaymentOwed() << endl;
+        }
+        else
+        {
+            cout << payment.getName() << ", doesn't have to do anything." << endl;
+        }
+    }
 
     return 0;
 }
