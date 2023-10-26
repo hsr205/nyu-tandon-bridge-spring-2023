5af8a75abd218001f10a8feacdbb9f3dc75cff25
Henry Rothenberg <hsr205@aol.com>
2023-10-26 13:47:54 -0400

    WIP hw16_pt2

--- All_Weeks/Week_16/C++_Programs/hr2600_hw16_pt2.cpp
+++ All_Weeks/Week_16/C++_Programs/hr2600_hw16_pt2.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+template <typename T>
+class Queue
+{
+
+private:
+    int index = 0;
+    vector<T> dataVector;
+
+public:
+    Queue() = default;
+    ~Queue() = default;
+
+    void push(T newValue)
+    {
+        dataVector.push_back(newValue);
+    }
+
+    void pop()
+    {
+        ++index;
+        dataVector.insert(dataVector.begin(), dataVector.begin() + index, dataVector.begin() + dataVector.size());
+    }
+
+    bool isEmpty()
+    {
+        return dataVector.size() == 0;
+    }
+
+    void resetVector()
+    {
+        if (isEmpty(dataVector))
+        {
+            dataVector.resize(1);
+        }
+    }
+};
+
 int main()
 {
-    cout << "Hello from " << __FILE__ << endl;
+
+    Queue<int> dataQueue;
+
+    dataQueue.push(3);
+
+    while(!dataQueue.isEmpty()){
+
+
+
+    }
+
     return 0;
 }
